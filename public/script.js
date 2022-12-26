f3de13797e40eda672fb87d4a65d6d84d04793bf
Nguyễn Trường Lâm <lam.nguyen@wecheer.io>
2022-12-26 21:34:49 +0700

    add

--- public/script.js
+++ public/script.js
@@ -12,7 +12,8 @@ window.onload = function () {
     redirect: "follow",
   };
 
-  fetch("http://[::1]:3000/get_img", requestOptions)
+  // fetch("http://[::1]:3000/get_img", requestOptions)
+  fetch("http://35.240.171.212:3000/get_img", requestOptions)
     .then((response) => response.text())
     .then((result) => {
       const newRow = document.createElement("tr");
