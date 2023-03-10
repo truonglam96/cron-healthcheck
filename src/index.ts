035776f7a584589cce4634666263b69d65a4aa0e
Nguyễn Trường Lâm <lam.nguyen@wecheer.io>
2023-03-10 14:20:24 +0700

    update gracePeriodForClose from 5000 to 300000

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ if (require.main === module) {
       // (don't force-close). If you want to immediately destroy all sockets
       // upon stop, set its value to `0`.
       // See https://www.npmjs.com/package/stoppable
-      gracePeriodForClose: 5000, // 5 seconds
+      gracePeriodForClose: 300000, // 5 seconds
       openApiSpec: {
         // useful when used with OpenAPI-to-GraphQL to locate your application
         setServersFromRequest: true,
